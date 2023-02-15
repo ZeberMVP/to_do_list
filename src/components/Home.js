d9083addd5b05452f54fb926966357702026a5f9
ZeberMVP <rubeen722@gmail.com>
2023-02-15 16:21:30 +0100

    add/home route

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+    return ( 
+        <div className="todoapp stack-large">
+            <h1>TodoMatic</h1>
+            <h2>Welcome</h2>
+            <a href="/to-do-list">To Do List</a>
+        </div>
+    )
+}
+
+export default Home;
